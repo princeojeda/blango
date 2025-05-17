04b2e2dde3dc2ee8ee0634e50faf0b3edd7227fa
Prince Jeorge Ojeda <princeojeda52@gmail.com>
2025-05-17 11:07:01 +0000

    Finish custom filters

--- blango/templates/blog/views.py
+++ blango/templates/blog/views.py
@@ -1,2 +1,6 @@
+from django.utils import timezone
+from blog.models import Post
+
 def index(request):
-    return render(request, "blog/index.html")
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
